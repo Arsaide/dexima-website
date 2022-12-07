9f099f8ec8d7e0aa2ca9554bfa95d39eb2651e1a
Kirill <93765186+Arsaide@users.noreply.github.com>
2022-12-07 23:38:00 +0200

    Add porfolio, order pages. Update team pages. Add JS form for order pages

--- js/app.js
+++ js/app.js
@@ -48,14 +48,80 @@
             }));
         }
     }), 0);
-    const smoothLinks = document.querySelectorAll('a[href^="#"]');
-    for (let smoothLink of smoothLinks) smoothLink.addEventListener("click", (function(e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute("href");
-        document.querySelector(id).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
+    document.addEventListener("DOMContentLoaded", (function() {
+        const form = document.getElementById("form");
+        form.addEventListener("submit", formSend);
+        async function formSend(e) {
+            e.preventDefault();
+            let error = formValidate(form);
+            let formData = new FormData(form);
+            formData.append("file", formFile.files[0]);
+            if (0 === error) {
+                form.classList.add("_sending");
+                let response = await fetch("sendmail.php", {
+                    method: "POST",
+                    body: formData
+                });
+                if (response.ok) {
+                    let result = await response.json();
+                    alert(result.message);
+                    formPreview.innerHTML = "";
+                    form.reset();
+                    form.classList.remove("_sending");
+                } else {
+                    alert("Помилка!");
+                    form.classList.remove("_sending");
+                }
+            } else alert("Заповніть обов'язкові поля!!!");
+        }
+        function formValidate(form) {
+            let error = 0;
+            let formReq = document.querySelectorAll("._req");
+            for (let index = 0; index < formReq.length; index++) {
+                const input = formReq[index];
+                formRemoveError(input);
+                if (input.classList.contains("_email")) {
+                    if (emailTest(input)) {
+                        formAddError(input);
+                        error++;
+                    }
+                } else if ("checkbox" === input.getAttribute("type") && false === input.checked) {
+                    formAddError(input);
+                    error++;
+                } else if ("" === input.value) {
+                    formAddError(input);
+                    error++;
+                }
+            }
+            return error;
+        }
+        function formAddError(input) {
+            input.parentElement.classList.add("_error");
+            input.classList.add("_error");
+        }
+        function formRemoveError(input) {
+            input.parentElement.classList.remove("_error");
+            input.classList.remove("_error");
+        }
+        function emailTest(input) {
+            return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+        }
+        const formFile = document.getElementById("formFile");
+        const formPreview = document.getElementById("formPreview");
+        formFile.addEventListener("change", (() => {
+            uploadFile(formFile.files[0]);
+        }));
+        function uploadFile(file) {
+            var reader = new FileReader;
+            reader.onload = function(e) {
+                formPreview.innerHTML = `<span>${JSON.stringify(e.target.file)}</span>`;
+                JSON.stringify(e.target);
+            };
+            reader.onerror = function(e) {
+                alert("Помилка");
+            };
+            reader.readAsDataURL(file);
+        }
     }));
     window["FLS"] = true;
     isWebp();
